588d50aeb286c5ead7474ae006a9198d44f8799b
krishnasamudre <samudrekrishna96@gmail.com>
2022-03-17 17:15:07 +0530

    10th commit

---
+++ run.sh
@@ -0,0 +1,37 @@
+#run.sh 
+mkdir $1
+cd $1
+mkdir uploads 
+cd uploads
+mkdir accidents
+cd  accidents
+mkdir images
+mkdir signatures
+cd ..
+mkdir inductiontrainings
+cd inductiontrainings 
+mkdir signatures
+mkdir documents
+cd ..
+mkdir issues
+cd issues
+mkdir images
+mkdir signatures
+cd ..
+mkdir workpermit
+cd workpermit
+mkdir documents
+mkdir signatures
+cd ..
+mkdir toolboxtraining
+cd toolboxtraining
+mkdir groupphotos
+mkdir signatures
+mkdir documents
+cd ..
+cd ..
+mkdir downloads
+cd uploads
+mkdir inductiontrainings
+echo "--- $1 successfully created ---"
+
